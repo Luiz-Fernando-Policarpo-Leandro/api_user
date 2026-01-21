8faf58710cbe84a5e1ec8114eda6b384ddc6201e
Luiz-Fernando-Policarpo-Leandro <fasfernandoal@outlook.com.br>
2026-01-21 01:22:20 -0300

    user_update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,54 @@
 class UsersController < ApplicationController
-    before_action :authorize, except: %i[ login create ]
+  before_action :authorize, except: %i[login create]
+  before_action :set_user, only: %i[show update]
 
-    def login
-        user = User.find_by(email: user_params[:email])
-        if user && user.authenticate(user_params[:password])
-            token = encode_code(user_id: user.id)
-            render json: {user: user, token: token}, status: :ok
-        else
-            render json: {erro: "Credenciais invalidas"}, status: :unprocessable_entity
-        end
+  def login
+    user = User.find_by(email: user_params[:email])
+
+    if user&.authenticate(user_params[:password])
+      token = encode_code(user_id: user.id)
+      render json: { user: user, token: token }, status: :ok
+    else
+      render json: { erro: "Credenciais inválidas" }, status: :unauthorized
     end
+  end
 
-    def index
+  def index
+    users = User.all
+    render json: users.as_json(only: [:id, :nome, :email]), status: :ok
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      token = encode_code(user_id: user.id)
+      render json: { user: user, token: token }, status: :created
+    else
+      render json: { erro: user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def create
-        user = User.new(user_params)
-        if user.save!
-            token = encode_code(user_id: user.id)
-            render json: {user: @user ,"token": token}, status: :ok
-        else
-            render json: {"erro": user.errors}, status: :unprocessable_entity
-        end
+  def show
+    render json: { nome: @user.nome, email: @user.email }, status: :ok
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: { message: "Usuário atualizado com sucesso", user: @user }, status: :ok
+    else
+      render json: { erro: @user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def show
-        id_user = params[:id] || nil
-        if id_user
-            res = [User.find_by(id: id_user)]
-        else
-            res = User.all
-        end
+  private
 
-        render json: res.map {|user| {nome: user.nome, email: user.email} }, status: :ok
-    end
+  def set_user
+    @user = User.find_by(id: params[:id])
+    render json: { erro: "Usuário não encontrado" }, status: :not_found unless @user
+  end
 
-    def update
-
-    end
-
-    private
-
-    def user_params
-        params.permit(:nome, :email, :password)
-    end
-
+  def user_params
+    params.permit(:nome, :email, :password)
+  end
 end
